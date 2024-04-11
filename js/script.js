45cb1edf991e35eb52c2b6f9b1078c5f666e4ac8
AlonaRahilevych <ragilevic@gmail.com>
2024-04-11 11:38:19 +0200

    function createElem was changed and functions createNavLinks and createListElem were added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 createNav(createElem);
 createWrapper(createElem);
+createNavLinks(createElem);
 
 function createElem(parentElem, type, className, content) {
   const newElem = document.createElement(type);
   newElem.innerText = content;
-  newElem.className = className;
+  for (let i = 0; i < className.length; i++) {
+    newElem.classList.add(className[i]);
+  }
+
   parentElem.appendChild(newElem);
 }
 function createWrapper(createElem) {
-  const wrapper = document.querySelector('.nav__block');
+  const parentElem = document.querySelector('.nav__block');
   const type = 'div';
-  const className = 'container';
+  const className = ['container', 'nav__container'];
   const content = null;
-  createElem(wrapper, type, className, content);
+  createElem(parentElem, type, className, content);
 }
 function createNav(createElem) {
-  const wrapper = document.querySelector('.wrapper');
+  const parentElem = document.querySelector('.wrapper');
   const type = 'nav';
-  const className = 'nav__block';
+  const className = ['nav__block'];
   const content = null;
-  createElem(wrapper, type, className, content);
+  createElem(parentElem, type, className, content);
+}
+function createListElem(createElem) {
+  const parentElem = document.querySelector('.nav__container');
+  const type = 'li';
+  const className = ['nav__list'];
+  const content = null;
+  createElem(parentElem, type, className, content);
+}
+function createNavLinks(createElem) {
+  const parentElem = document.querySelector('.nav__container');
+  const type = 'ul';
+  const className = ['nav', 'justify-content-end'];
+  const content = null;
+  createElem(parentElem, type, className, content);
 }
